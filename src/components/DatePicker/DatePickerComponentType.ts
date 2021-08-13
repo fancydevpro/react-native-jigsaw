0d08ca933e41a18c5a01610993f348ea0471c660
fancydevpro <41801970+fancydevpro@users.noreply.github.com>
2021-08-13 18:22:24 -0400

    extended DatePickerComponentProps with the original datetimepicker component

--- src/components/DatePicker/DatePickerComponentType.ts
+++ src/components/DatePicker/DatePickerComponentType.ts
@@ -1,10 +1,11 @@
+import { IOSNativeProps, AndroidNativeProps, WindowsNativeProps } from "@react-native-community/datetimepicker";
 import theme from "../../styles/DefaultTheme";
 
-export interface DatePickerComponentProps {
+export type DatePickerComponentProps = IOSNativeProps & AndroidNativeProps & WindowsNativeProps & {
   value: Date;
   onChange: (e: any, data?: any) => void;
   mode: "date" | "time" | "datetime";
   toggleVisibility: () => void;
   isVisible?: boolean;
   theme?: typeof theme;
-}
+};
